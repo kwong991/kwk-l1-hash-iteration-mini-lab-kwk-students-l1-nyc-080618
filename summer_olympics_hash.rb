36771b2161ccea63bdfe94d0482840e573eeb4f9
kwong991 <kgwong991@gmail.com>
2018-08-08 19:49:17 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,31 +1,31 @@
 
-def create_olympics_hash
-  {Sydney: "2000",
-    Athens: "2004",
-    Beijing: "2008",
-    London: "2012"}
+# def create_olympics_hash
+#   {Sydney: "2000",
+#     Athens: "2004",
+#     Beijing: "2008",
+#     London: "2012"}
     
-end
+# end
 
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-  new_hash = create_olyimpics_hash
-  new_hash [:Atlanta] = "1996"
-  new_hash
-end
+# def add_a_key_value_pair
+#   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+#   new_hash = create_olyimpics_hash
+#   new_hash [:Atlanta] = "1996"
+#   new_hash
+# end
 
-def iterate_through_hash
-  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+# def iterate_through_hash
+#   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
+#   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
   
-add_a_key_value_pair.each do {key, value}
-puts "{the #{value} summer olyimpics took place in #{key}}"
+# add_a_key_value_pair.each do {key, value}
+# puts "{the #{value} summer olyimpics took place in #{key}}"
   
-end
+# end
 
-def iterate_through_keys
-  # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-  upcased_cities = add_a_key_value_pair.map {|key,value| key.upcase}
-  upcased_cities.each{|city| puts city}
-end
+# def iterate_through_keys
+#   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
+#   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+#   upcased_cities = add_a_key_value_pair.map {|key,value| key.upcase}
+#   upcased_cities.each{|city| puts city}
+# end
